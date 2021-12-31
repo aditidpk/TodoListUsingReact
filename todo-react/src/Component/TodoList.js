3772daa74d6d7d26ddf6c2059eae5303c93dfe00
aditideepak <aditi.dpk17@gmail.com>
2021-12-31 10:56:58 +0530

    Added changes

--- todo-react/src/Component/TodoList.js
+++ todo-react/src/Component/TodoList.js
@@ -9,17 +9,22 @@ export default class TodoList extends Component {
       let list = this.state.list;
       list.push(this.state.newItem);
       this.setState({list})
+      this.state.newItem.value=''
   }
   render() {
     return (
       <div>
-        Enter item:
+        <h1>Todo List</h1>
+        What needs to be done:
         <input
           type="text"
           onChange={(e) => this.setState({ newItem:e.target.value })}
           name="name"
         />
-        <button onClick={this.addItemToList}>Add item</button>
+        <button onClick={this.addItemToList}>Add</button>
+        <text>
+        <br/>Tasks in Todo
+        </text>
         {this.state.list.map((e, index) => {
           return (
             <div key={index}>
